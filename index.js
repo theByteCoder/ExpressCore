60caa9d04aac7d4af27f75c7badb40e1f7ba04eb
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-03-03 00:25:06 +0530

    Added POST PUT Middlewires

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Member')
 const logger = require('./middlewire/logger')
 
 const app = express();
@@ -8,13 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // init middlewire
-app.use(logger);
+// app.use(logger);
+
+// body parser middlewire
+app.use(express.json());
+
+// form submission middlewire
+app.use(express.urlencoded({ extended: false }));
 
 // set static folder using middlewire
 app.use(express.static(path.join(__dirname, 'static')));
 
-// get all members
-app.get('/api/jsonresponse', (req, res) => res.json(members));
+app.use('/api/members', require('./routes/api/member'))
 
 app.listen(PORT, () => {
     console.log(`Express server on PORT ${PORT}`);
---
+++ routes/api/member.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const members = require('../../Member')
+const uuid = require('uuid')
+
+// get all members
+router.get('/', (req, res) => res.json(members));
+
+// get single members
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const found = members.some(member => member.id === parseInt(id));
+    if (found) {
+        res.json(members.filter((member) => member.id === parseInt(id)))
+    } else {
+        res.status(400).json({ msg: `Id ${id} not found` });
+    }
+    res.json()
+});
+
+// create member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active',
+    }
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({ msg: 'name and email are mandatory' })
+    }
+    members.push(newMember);
+    res.json(members);
+})
+
+// update member
+// get single members
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const found = members.some(member => member.id === parseInt(id));
+    if (found) {
+        const updateMmber = req.body;
+        members.forEach((member) => {
+            if (member.id === parseInt(id)) {
+                member.name = updateMmber.name ? updateMmber.name : member.name;
+                member.email = updateMmber.email ? updateMmber.email : member.email;
+                res.json({ msg: 'Member is updated', member })
+            }
+        })
+        res.json(members.filter((member) => member.id === parseInt(id)))
+    } else {
+        res.status(400).json({ msg: `Id ${id} not found` });
+    }
+    res.json()
+});
+
+module.exports = router;
